d09b4e8ff7a43161d09ebf7bbd4021eb9910049e
ImmArkorful <e.arkorful3@gmail.com>
2022-06-07 00:57:54 +0000

    virtualenv base

--- deploy_v2/update.sh
+++ deploy_v2/update.sh
@@ -6,8 +6,8 @@ PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 VIRTUALENV_BASE_PATH='/usr/local/virtualenvs'
 
 git pull
-$VIRTUALENV_BASE_PATH/env/bin/python manage.py migrate
-$VIRTUALENV_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
 supervisorctl restart profiles_api
 
 echo "DONE! :)"
